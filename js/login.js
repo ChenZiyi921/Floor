3704cca0540ce99da998214dfd1a5606bdba7574
chenziyi <chenziyi921@gmail.com>
2023-09-12 00:08:00 +0800

    登录页面

---
+++ js/login.js
@@ -0,0 +1,34 @@
+function toStatistics() {
+    location.href = './statistics.html';
+}
+
+function toQuery() {
+    location.href = './project_list.html';
+}
+
+function submit() {
+    var username = document.querySelector('.username');
+    var password = document.querySelector('.password');
+    var login_submit = document.querySelector('.login_submit');
+    login_submit.addEventListener('click', function () {
+        login({ username: username, password: password })
+    })
+}
+
+function login(values) {
+    $.ajax({
+        type: "post",
+        url: base_url + 'api/v10/login',
+        data: values,
+        dataType: "json",
+        success: function (res) {
+            if (res.status === 'success') {
+                // location.href = '';
+            }
+        },
+    });
+}
+
+document.addEventListener('click', function () {
+
+})
