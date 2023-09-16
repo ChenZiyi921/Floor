62b8aaff9979610eb4c1ed7ee85cace2d084c4ef
chenziyi <chenziyi921@gmail.com>
2023-09-16 15:47:43 +0800

    地块位置、户型信息

--- mini-program/miniprogram/pages/info/info.ts
+++ mini-program/miniprogram/pages/info/info.ts
@@ -2,9 +2,24 @@ Page({
   data: {
     logs: [],
   },
+  toRoomDistribution() {
+    wx.navigateTo({
+      url: '../room_distribution/room_distribution',
+    })
+  },
   toRoomVideo() {
     wx.navigateTo({
       url: '../room_video/room_video',
+    })
+  },
+  toRoomPosition() {
+    wx.navigateTo({
+      url: '../room_position/room_position',
+    })
+  },
+  toRoomType() {
+    wx.navigateTo({
+      url: '../room_type/room_type',
     })
   },
   onLoad() {},
