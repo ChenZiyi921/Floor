a0e33628ceb5526b2dd245a2b5ea15a067b4f0dd
Chenziyi <chenziyi921@gmail.com>
2024-09-23 10:59:46 +0800

    rename

--- mini-program/typings/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="./types/index.d.ts" />
-
-interface IAppOption {
-  globalData: {
-    userInfo?: WechatMiniprogram.UserInfo;
-    host: string;
-  };
-  userInfoReadyCallback?: WechatMiniprogram.GetUserInfoSuccessCallback;
-}
